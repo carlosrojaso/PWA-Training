6b750e6ea1be00b4baeecce9a6b816aa84bf5893
Carlos Rojas <Carlos.Y.Rojas.-ND@disney.com>
2018-11-22 15:32:58 -0500

    adding onsen in index

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
-import { Greeter } from './greeter';
+import ons from 'onsenui';
 
-const g = new Greeter('Juri');
-g.greet();
+document.addEventListener('init', function (event: any) {
+
+  var page = event.target;
+
+  if (page.id === 'page1') {
+    page.querySelector('#push-button').onclick = function() {
+      // document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Page 2'}});
+    };
+  } else if (page.id === 'page2') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  }
+
+});
